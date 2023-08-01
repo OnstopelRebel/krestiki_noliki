ba9724f4309dc7e32697cde2b4cdb972f970dd1f
OnstopelRebel <onstorebel@gmail.com>
2023-08-01 17:03:36 +0300

    add eventlistener for gamefield

---
+++ src/js/module/listener.js
@@ -0,0 +1,17 @@
+const gameField = document.querySelector('.game-field')
+const gameCells = gameField.querySelectorAll('.game-field--cell')
+const sideChoiceX = document.querySelector('.side-choice--x')
+
+
+gameField.addEventListener('click', evt => {
+  const gameCellData = evt.target.dataset.cell
+  const gameCell = gameField.querySelector(`.game-field__cell--${gameCellData}`)
+
+  sideChoiceX.checked ? gameCell.classList.toggle('cross') : gameCell.classList.toggle('zero')
+
+
+
+
+  console.log(gameCell)
+})
+
